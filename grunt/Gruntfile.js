1792246e30a86ef6b7e343a9fe1253e0205ed411
basem909 <basemshams30@gmail.com>
2023-02-08 16:05:54 -0300

    feat: Setup the uglify plugin and configure it

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,3 +1,16 @@
 module.exports = (grunt) => {
-  
+  // Load the plug-in
+  grunt.loadNpmTask("grunt-contrib-uglify");
+
+  // Configure the plug-in
+  grunt.initConfig({
+    uglify: {
+      build: {
+        files : {
+          src: 'js/*.js',
+          dist: 'build/app.js'
+        }
+      }
+    }
+  })
 }
